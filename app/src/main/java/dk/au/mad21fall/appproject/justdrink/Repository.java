2a4d097f90f1b7b562770b69e5ac5a6d6d918f61
Jesper <201810577@post.au.dk>
2021-11-25 14:59:08 +0100

    Repository update (Creating, making repository)

--- app/src/main/java/dk/au/mad21fall/appproject/justdrink/Repository.java
+++ app/src/main/java/dk/au/mad21fall/appproject/justdrink/Repository.java
@@ -1,13 +1,24 @@
 package dk.au.mad21fall.appproject.justdrink;
 
+import android.app.Application;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
+import java.util.List;
+
+//Creating repository
 class Repository {
     private static final Repository ourInstance = new Repository();
     private FirebaseDatabase mDatabase;
     private FirebaseAuth mAuth;
-
+    private FirebaseStorage mStorage;
+    private LiveData<List<String>> getText;
     static Repository getInstance() {
         return ourInstance;
     }
@@ -15,6 +26,11 @@ class Repository {
     private Repository() {
         mDatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        mStorage = FirebaseStorage.getInstance();
     }
 
+    //Making repository
+    public Repository(Application application){
+
+    }
 }
