5d62c36265c24ef66d389c84137cc078f43c9eb3
Jesper Hansen <201810577@post.au.dk>
2021-12-04 20:10:09 +0100

    Detail update

--- app/src/main/java/dk/au/mad21fall/appproject/justdrink/ViewModel/DetailedViewViewModel.java
+++ app/src/main/java/dk/au/mad21fall/appproject/justdrink/ViewModel/DetailedViewViewModel.java
@@ -3,16 +3,22 @@ package dk.au.mad21fall.appproject.justdrink.ViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.FirebaseDatabase;
 
 import dk.au.mad21fall.appproject.justdrink.Model.Location;
 import dk.au.mad21fall.appproject.justdrink.Model.OpenHours;
+import dk.au.mad21fall.appproject.justdrink.View.DetailedViewFragment;
 import dk.au.mad21fall.appproject.justdrink.View.StorageActivity;
 
 public class DetailedViewViewModel extends ViewModel {
     // TODO: Implement the ViewModel
 
+    public DetailedViewFragment DetailView;
     public MutableLiveData<Location> mLocation;
+
+    private LatLng position;
+
 
 
 
